44ff059930101c58a82414afbcb780c0c1920412
Luis Gerardo Gonzalez Delgado <luisge1299@gmail.com>
2025-09-28 14:41:48 -0400

    cambios para subir a la web2

--- Back/index.js
+++ Back/index.js
@@ -219,8 +219,6 @@ app.get('/event/:id', async (req, res) => {
   }
 });
 
-// Al final del archivo, cambia:
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
